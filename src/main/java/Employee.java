e1ee77a3283237e2819b6dbf4156b651c3e883b4
unknown <raje1reddy@gmail.com>
2019-08-16 17:50:46 +0100

    objects fields tests

--- src/main/java/Employee.java
+++ src/main/java/Employee.java
@@ -1,11 +1,15 @@
 
 class Employee {
-    String name;
-    int age;
-    Employee(){
-        age = 20;
-        System.out.println("user defined constructor");
-
+   double age;
+//Setter method set the value of the variable
+    public void setAge(double newAge) {
+        age = newAge;
     }
+//Getter method to get the value of the variable
+    public double getAge() {
+        return age;
+    }
+
+
 }
 
--- src/test/java/EmployeeTest.java
+++ src/test/java/EmployeeTest.java
@@ -5,6 +5,10 @@ public class EmployeeTest {
     public static void main (String args[]) {
         Employee emp = new Employee();
         System.out.println(emp.age);
+        Employee emp1 = new Employee();
+         emp1.setAge(38.59);
+         double age1 = emp1.getAge();
+         System.out.println(age1);
     }
 
 }
